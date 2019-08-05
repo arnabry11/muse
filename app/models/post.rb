2921c2022172645794b30ccc859c79254cc97303
ARNAB ROY <rarnab021@gmail.com>
2019-08-05 13:53:37 +0530

    PaperClip setup done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :title, :link, :description, presence: true
+  has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
+  validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
