d31f740c43c0069c4be5abff4c24fa079fd6f148
ARNAB ROY <rarnab021@gmail.com>
2019-08-05 14:41:32 +0530

    Added Comments to Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :title, :link, :description, presence: true
   has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
   validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
